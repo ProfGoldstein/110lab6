d0bbda5588958b502d42a5356209004e454faea4
Ira Goldstein <igoldstein@siena.edu>
2025-11-02 13:23:54 -0500

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# Ira Goldstein
+# CSIS110 Lab 6
+
+# Python program that displays the traditional Hello World
+
+# Print out the required starting HTML
+print("Content-Type: text/html\n")
+
+# Print out the minimal opening tags
+print("<html><body>")
+
+# Display the text
+print("Hello World")
+
+
+# Closing HTML tags
+print("</body></html>")
